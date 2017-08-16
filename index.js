1e04309d5c962d05c68dc186feb411f723fdc0f8
itreverie <brendadhk@gmail.com>
2017-08-15 23:46:57 -0400

    Adding first version of palindromes

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-window.onload = function(e) {
+
 
     function solveProblem() {
         var inputCharacters = document.getElementById('inputCharacters').value;
-
-        alert(inputCharacters);
+        document.getElementById('result').innerHTML=isAnyPermutationPalindrome(inputCharacters);
     }
 
+    var letterConcurrency={
+        concurrency:0,
+        letter:'',
+        isEven: function (){this.concurrency %2 ===0 ? true : false}
+    };
 
-}
+    function isAnyPermutationPalindrome(word)
+    {
+        var wordCharacters=Array.from(word);
+        var arrayOfLetters=[];
+
+        wordCharacters.forEach(function(letter){
+
+            //console.log(letter);
+             if(arrayOfLetters.hasOwnProperty(letter))
+             {
+                 arrayOfLetters[letter] = arrayOfLetters[letter]+1;
+             }
+             else
+             {
+                 //  var letterCurrency= new Object();
+                 //  letterCurrency.letter = letter;
+                 //  letterCurrency.concurrency = 1;
+                 //
+                 // arrayOfLetters[letter] = Object.create(letterCurrency);
+                 arrayOfLetters[letter] = 1;
+             }
+
+           // console.log('key:'+ letter +' value: '+arrayOfLetters[letter]);
+
+         });
+
+        var numberOfUnevenLetters=0;
+
+
+        for(var key in arrayOfLetters)
+        {
+            if(arrayOfLetters[key]) {
+
+                console.log('K: ' + key +' V:'+ arrayOfLetters[key]);
+
+                //If it is uneven
+                if(arrayOfLetters[key] % 2 !== 0)
+                {
+                    numberOfUnevenLetters +=1;
+                }
+            }
+        }
+
+
+        return (numberOfUnevenLetters > 1) ? false : true;
+    }
+
