cb5492ce3a8d7de526e804ff45af401db098e966
itreverie <brendadhk@gmail.com>
2017-08-16 07:51:21 -0400

    Adding a dictionary solution

--- index.js
+++ index.js
@@ -5,53 +5,34 @@
         document.getElementById('result').innerHTML=isAnyPermutationPalindrome(inputCharacters);
     }
 
-    var letterConcurrency={
-        concurrency:0,
-        letter:'',
-        isEven: function (){this.concurrency %2 ===0 ? true : false}
-    };
-
     function isAnyPermutationPalindrome(word)
     {
         var wordCharacters=Array.from(word);
         var arrayOfLetters=[];
+        var numberOfUnevenLetters=0;
 
         wordCharacters.forEach(function(letter){
-
-            //console.log(letter);
-             if(arrayOfLetters.hasOwnProperty(letter))
-             {
-                 arrayOfLetters[letter] = arrayOfLetters[letter]+1;
-             }
-             else
-             {
-                 //  var letterCurrency= new Object();
-                 //  letterCurrency.letter = letter;
-                 //  letterCurrency.concurrency = 1;
-                 //
-                 // arrayOfLetters[letter] = Object.create(letterCurrency);
-                 arrayOfLetters[letter] = 1;
-             }
-
-           // console.log('key:'+ letter +' value: '+arrayOfLetters[letter]);
+             (arrayOfLetters.hasOwnProperty(letter)) ? arrayOfLetters[letter] = arrayOfLetters[letter] + 1
+                                                     : arrayOfLetters[letter] = 1;
+            //console.log('key:'+ letter +' value: '+arrayOfLetters[letter]);
 
          });
-
-        var numberOfUnevenLetters=0;
 
 
         for(var key in arrayOfLetters)
         {
-            if(arrayOfLetters[key]) {
+             //console.log(key);
+            if(numberOfUnevenLetters > 1){
+                break;
+            }
 
-                console.log('K: ' + key +' V:'+ arrayOfLetters[key]);
-
+            //if(arrayOfLetters[key]) {
                 //If it is uneven
                 if(arrayOfLetters[key] % 2 !== 0)
                 {
                     numberOfUnevenLetters +=1;
                 }
-            }
+            //}
         }
 
 
